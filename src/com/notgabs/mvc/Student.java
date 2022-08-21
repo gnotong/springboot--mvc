25a08411f0f9312e12e679eead758e1a3a5ef1a5
lineDjuissi <linedjuissi@yahoo.com>
2022-08-21 02:31:59 +0200

    spring-mvc: select form tags filled with properties file

--- src/com/notgabs/mvc/Student.java
+++ src/com/notgabs/mvc/Student.java
@@ -1,20 +1,9 @@
 package com.notgabs.mvc;
 
-import java.util.LinkedHashMap;
-
 public class Student {
 	private String firstName;
 	private String LastName;
-	private String country;
-	private LinkedHashMap<String, String> countryOptions;
-	
-	public Student() {
-		countryOptions = new LinkedHashMap<>();
-		countryOptions.put("CMR", "Cameroon");
-		countryOptions.put("CI", "Ivory Coast");
-		countryOptions.put("BN", "Benin");
-	}
-	
+	private String country;	
 	
 	public String getFirstName() {
 		return firstName;
@@ -33,8 +22,5 @@ public class Student {
 	}
 	public void setCountry(String country) {
 		this.country = country;
-	}
-	public LinkedHashMap<String, String> getCountryOptions() {
-		return countryOptions;
 	}
 }
--- src/com/notgabs/mvc/StudentController.java
+++ src/com/notgabs/mvc/StudentController.java
@@ -1,5 +1,8 @@
 package com.notgabs.mvc;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -8,6 +11,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/student")
 public class StudentController {
+	
+	@Value("#{countryOptions}")
+	private Map<String, String> countryOptions;
 
 	@RequestMapping("/showForm")
 	public String showForm(Model model) {
@@ -15,6 +21,7 @@ public class StudentController {
 		Student student = new Student();
 		
 		model.addAttribute("student", student);
+		model.addAttribute("countryOptions", countryOptions);
 		
 		return "student-form";
 	}
